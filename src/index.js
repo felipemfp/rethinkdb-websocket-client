0e8bc3195b3e43611e0c74343ca2be73a929b4d2
Abhishek Patil <abhishek@supportlogic.io>
2018-10-10 13:51:40 +1100

    Revert "user and password support to rdb websock"
    This reverts commit 4a235720fb69d284c157cd2b1b9e58dfbc329c63.

--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ import rethinkdb from 'rethinkdb';
 import protodef from 'rethinkdb/proto-def';
 import {configureTcpPolyfill} from './TcpPolyfill';
 
-function connect({host, port, path, secure, wsProtocols, db, simulatedLatencyMs, user, password}) {
+function connect({host, port, path, secure, wsProtocols, db, simulatedLatencyMs}) {
   configureTcpPolyfill({path, secure, wsProtocols, simulatedLatencyMs});
   // Temporarily unset process.browser so rethinkdb uses a TcpConnection
   const oldProcessDotBrowser = process.browser;
   process.browser = false;
-  
-  if(user == undefined) {
-    user = 'default'
-  }
-  if(password == undefined) {
-    password = ''
-  }
-
-  const connectOptions = {host, port, db, user, password};
+  const connectOptions = {host, port, db};
   const connectionPromise = Promise.promisify(rethinkdb.connect)(connectOptions);
   process.browser = oldProcessDotBrowser;
   return connectionPromise;
