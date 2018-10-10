98cb7afcc9285e054e84ecc4fc8f32769c720701
Abhishek Patil <abhishek@supportlogic.io>
2018-10-10 13:55:15 +1100

    back to default user settings
    Signed-off-by: Abhishek Patil <abhishek@supportlogic.io>

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import rethinkdb from 'rethinkdb';
 import protodef from 'rethinkdb/proto-def';
 import {configureTcpPolyfill} from './TcpPolyfill';
 
-function connect({host, port, path, secure, wsProtocols, wsBinaryType, db, simulatedLatencyMs}) {
+function connect({host, port, path, secure, wsProtocols, wsBinaryType, db, simulatedLatencyMs, user, password}) {
   configureTcpPolyfill({path, secure, wsProtocols, wsBinaryType, simulatedLatencyMs});
   // Temporarily unset process.browser so rethinkdb uses a TcpConnection
   const oldProcessDotBrowser = process.browser;
   process.browser = false;
-  const connectOptions = {host, port, db};
+  if(user === undefined) {
+    user = 'default'
+  }
+  if(password === undefined) {
+    password = ''
+  }
+  const connectOptions = {host, port, db, user, password};
   const connectionPromise = Promise.promisify(rethinkdb.connect)(connectOptions);
   process.browser = oldProcessDotBrowser;
   return connectionPromise;
